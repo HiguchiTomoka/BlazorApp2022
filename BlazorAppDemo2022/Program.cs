8f693dba7e2d85ade447a85b7d17d7f1714c1f4d
友香 樋口 <to-higuchi@fusion-com.jp>
2026-05-08 10:16:18 +0900

    circuitテストの再現

--- BlazorAppDemo2022/Program.cs
+++ BlazorAppDemo2022/Program.cs
@@ -1,10 +1,15 @@
 using BlazorAppDemo2022.Components;
+using BlazorAppDemo2022.Components.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// DI注入
+// AddRazorComponentsメソッド(コンポーネントを登録、ルーティング・レンダリング基盤を作る)
+// AddInteractiveServerComponentsメソッド(BlazorServerを使用可能に。イベントや状態保持をサーバーで有効化、動的UIが可能になる)
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+// DIコンテナに登録
+builder.Services.AddScoped<LocalStorageService>();
 
 var app = builder.Build();
 
@@ -16,12 +21,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// HTTPをHTTPSに強制リダイレクト 
 app.UseHttpsRedirection();
 
+// 静的ファイルの配信設定
 app.UseStaticFiles();
+
+// CSRF対策
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+// ルーティング登録,インタラクティブ機能の登録
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
